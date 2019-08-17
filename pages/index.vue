7bcac4ca935dd5ab1334e54e9dfeec57eb41c966
Ishizuka427 <a_ishizuka@codmon.co.jp>
2019-08-17 20:25:53 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,9 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Rabbit-game
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script>
-import Logo from '~/components/Logo.vue'
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Rabbit-game</title>
 
-export default {
-  components: {
-    Logo
-  }
-}
 </script>
 
 <style>
