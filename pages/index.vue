e9e6337ab8913c6f878cb8b666697805baa85d7e
Ishizuka427 <a_ishizuka@codmon.co.jp>
2019-08-18 12:21:51 +0900

    nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Rabbit-game
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <p>POINT: <span>{{ point }}</span></p>
+    <button v-on:click="pointUp">UP</button>
+    <button v-on:click="pointDown">DOWN</button>
+    <button onclick="location.reload()">CONTINUE</button>
+
+    <div class="game">
+
+      <div class="box"
+       v-on:click="answer(index)"
+       v-bind:class="{win: (answered && isWinner(index)), lose: (answered && !isWinner(index))}"
+       v-for="(answered, index) in answeredStatuses">
       </div>
+
+
     </div>
+    <button onclick="localStorage.removeItem('point'); point=0;pointTag.textContent = point;">Reset</button>
+
   </div>
+
+
+
 </template>
 
 <script>
@@ -31,39 +29,84 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
-  }
+  },
+  data () {
+    return {
+        point: 0,
+        winner: Math.floor(Math.random() * 5),
+        answeredStatuses: [false, false, false, false, false],
+    }
+  },
+  methods: {
+   isWinner (index) {
+       return this.winner == index;
+   },
+   answer (index) {
+       if (this.answeredStatuses[index]) {
+         return
+       }
+       this.answeredStatuses[index] = true;
+       if (this.isWinner(index)) {
+       this.pointUp();
+       } else {
+       this.pointDown();
+       }
+       this.answeredStatuses = Array.from(this.answeredStatuses);
+   },
+    pointUp () {
+       this.point += 5;
+    },
+     pointDown () {
+       this.point -= 1;
+    },
+  },
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  body {
+    display: flex;
+  }
+
+  p {
   text-align: center;
-}
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .game {
+    left: 0px;
+    margin: auto;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .box {
+    width: 100px;
+    height: 100px;
+    background: skyblue;
+    cursor: pointer;
+    transition: 0.8s;
+    margin: 0 8px 8px 0;
+    text-align: center;
+    line-height: 100px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .win {
+    background-image: url("../assets/usagi.png");
+    background-size: 100px;
+    border-radius: 50%;
+    transform: rotate(360deg);
+  }
+
+  .win::after {
+    content: "うさぎ";
+  }
+
+  .lose {
+    background-image: url("../assets/yagi.png");
+    background-size: 100px;
+    border-radius: 50%;
+    transform: scale(0.9);
+  }
+
+  .lose::after {
+    content: "やぎ";
+  }
 </style>
